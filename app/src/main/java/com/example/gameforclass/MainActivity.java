39abd55efc387000ad83e82ead1945979cbc7f76
waterdoze <anqduong2003@gmail.com>
2021-01-09 19:11:23 -0600

    Testing

--- app/src/main/java/com/example/gameforclass/MainActivity.java
+++ app/src/main/java/com/example/gameforclass/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
