7f35062b4a235d2700918951375da3d4a4bf5d02
ThatSmelNice <57548098+ThatSmelNice@users.noreply.github.com>
2021-01-14 19:11:47 -0600

    Cleaned up buttons
    Cleaned up the button layout/methods. Sidebar should be pog now

--- app/src/main/java/com/example/gameforclass/TheGameplay.java
+++ app/src/main/java/com/example/gameforclass/TheGameplay.java
@@ -2,6 +2,7 @@ package com.example.gameforclass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
@@ -23,24 +24,22 @@ public class TheGameplay extends AppCompatActivity {
         difficulty = i.getIntExtra("difficulty", 0); //If data not received, 0 is returned
 
     }
-    public void openTab(View v){//If the expand button is pressed, this is called. Animation will be added soon
-        ImageButton bruh = (ImageButton)v;//Could be removed?
-        v.setVisibility(View.INVISIBLE);
-        ScrollView SideBar = findViewById(R.id.SideBar);            //Will have to reorganize to have instantiation at the beginning of the method
-        ScrollView BigSideBar = findViewById(R.id.BigSideBar);
-        ImageButton BigTabButton = findViewById(R.id.BigTabButton);
-        SideBar.setVisibility(View.INVISIBLE);
-        BigSideBar.setVisibility(View.VISIBLE);
-        BigTabButton.setVisibility(View.VISIBLE);
-    }
-    public void closeTab(View v){
-        ImageButton bigTab = (ImageButton)v;
-        v.setVisibility(View.INVISIBLE);
+
+    //SETVISIBILITY VALUES: 0 == VISIBLE      4 == INVISIBLE      8 == GONE
+    @SuppressLint("WrongConstant")//Allows the setVisibility to take values other than the constants
+    public void changeSideBar(View v){//If the expand button is pressed, this is called. Animation will be added soon
         ScrollView SideBar = findViewById(R.id.SideBar);
         ScrollView BigSideBar = findViewById(R.id.BigSideBar);
+        ImageButton SmallTabButton = findViewById(R.id.SmallTabButton);
         ImageButton BigTabButton = findViewById(R.id.BigTabButton);
-        SideBar.setVisibility(View.VISIBLE);
-        BigSideBar.setVisibility(View.INVISIBLE);
-        BigTabButton.setVisibility(View.INVISIBLE);
+
+        int setBig = 0; int setSmall = 8;
+        if(v.getId() == R.id.BigTabButton){//if this is the big tab button we pressed, then we want to make the big ones disappear
+            setBig ^= setSmall;
+        }
+        SmallTabButton.setVisibility(setSmall);//Gone == invisible but on steroids because it won't affect layout or be treated as existing when it's set to gone
+        SideBar.setVisibility(setSmall);
+        BigSideBar.setVisibility(setBig);
+        BigTabButton.setVisibility(setBig);
     }
 }
