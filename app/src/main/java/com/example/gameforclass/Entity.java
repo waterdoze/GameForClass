f870baaad2851b5027ece1681e3b03edb575a418
ThatSmelNice <57548098+ThatSmelNice@users.noreply.github.com>
2021-01-19 09:14:01 -0600

    j

--- app/src/main/java/com/example/gameforclass/Entity.java
+++ app/src/main/java/com/example/gameforclass/Entity.java
@@ -1,17 +1,18 @@
 package com.example.gameforclass;
 
-public class Entity {
-    public int x;
-    public int y;
+public abstract class Entity {//Maybe this class can hold the image for the entity instead of all of the lower classes
+    public int tileX;
+    public int tileY;
+    public int screenX;
+    public int screenY;
     public int type;
 
-    public Entity(int x, int y, int type){//0 == Tower, 1 == Enemy
-        this.x = x;
-        this.y = y;
+    public Entity(int tileX, int tileY, int type){//0 == Tower, 1 == Enemy
+        this.tileX = tileX;
+        this.tileY = tileY;
         this.type = type;
     }
 
-    public double distanceTo(Entity other){
-        return Math.sqrt( ( Math.abs( (double)(this.x-other.x) ) + Math.abs( (double)(this.y-other.y) ) ) );
-    }
+    public int getTileX(){return tileX;}
+    public int getTileY(){return tileY;}
 }
