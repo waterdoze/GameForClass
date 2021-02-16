1d703ba9f146fb5c432a0b494d106047d0088974
waterdoze <anqduong2003@gmail.com>
2021-02-15 23:24:49 -0600

    More Supportive Messages

--- app/src/main/java/com/example/gameforclass/animation/EmotionalSupport.java
+++ app/src/main/java/com/example/gameforclass/animation/EmotionalSupport.java
@@ -10,9 +10,12 @@ public class EmotionalSupport {
 
     private String[] messages = new String[] {
             "It's all going to be okay in the end. If it's not okay, it's not the end.",
-            "Your smile is my sunrise.",
-            "Soon, when all is well, you're going to look back on this period in your life and be so glad you never gave up",
-            "Someone out there loves you."
+            "Your smile is my sunrise :)",
+            "Soon, when all is well, you're going to look back on this period in your life and be so glad you never gave up.",
+            "Someone out there loves you.",
+            "You are braver than you believe, stronger than you seem, and smarter than you think!",
+            "If you hear a voice within you saying \"YOU CANNOT PAINT,\" then by all means paint, and that voice will be silenced.",
+            "People might forget what you said and people might forget what you did, but they will never forget how you made them feel",
     };
 
     public EmotionalSupport(Context context) {
@@ -20,7 +23,7 @@ public class EmotionalSupport {
     }
 
     public void displayMessage() {
-        Toast message = Toast.makeText(context, messages[counter], Toast.LENGTH_SHORT);
+        Toast message = Toast.makeText(context, messages[counter], Toast.LENGTH_LONG);
         message.show();
         counter++;
         if (counter == messages.length) {
